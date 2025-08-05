c6227ef0414d70423c5dadb8f6be4e16100edb9b
Mateo O. <orehurcomito@gmail.com>
2025-08-05 15:01:37 -0500

    add external package

---
+++ packages/almacen_api/lib/src/almacen_api.dart
@@ -0,0 +1,7 @@
+/// {@template almacen_api}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class AlmacenApi {
+  /// {@macro almacen_api}
+  const AlmacenApi();
+}
---
+++ packages/almacen_api/test/src/almacen_api_test.dart
@@ -0,0 +1,12 @@
+// Not required for test files
+// ignore_for_file: prefer_const_constructors
+import 'package:almacen_api/almacen_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('AlmacenApi', () {
+    test('can be instantiated', () {
+      expect(AlmacenApi(), isNotNull);
+    });
+  });
+}
